99f511d62e8d4322e4d6b9e1866b0d45545e4d57
Csaba Bartfai <csaba_bartfai@epam.com>
2022-07-10 23:49:27 +0200

    Parallax demo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ChapterTitle from './components/ChapterTitle/ChapterTitle';
 import data from './assets/content.json';
 import ChapterParagraph from './components/ChapterParagraph/ChapterParagraph';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import backgroundImg from './book-setting.jpg';
 import styled, { ThemeProvider } from 'styled-components';
 
 const theme = {
@@ -28,14 +30,35 @@ width: 100px;
 
 function App() {
   const { title, paragraphs } = data.chapters.at(0);
-  const [currentTheme, setCurrentTheme] = useState(theme.green)
+  const [currentTheme, setCurrentTheme] = useState(theme.green);
 
   return (
     <ThemeProvider theme={currentTheme} >
       <div className="ChapterReader">
-        <ChapterTitle title={title} />
-        <Button onClick={() => setCurrentTheme(currentTheme.name === 'Green' ? theme.blue : theme.green)}>{currentTheme.name}</Button>
-        {paragraphs.map((paragraph) => <ChapterParagraph key={paragraph.id} paragraph={paragraph.content} />)}
+        <Parallax pages={paragraphs.length / 2 + 1} style={{ top: '0', left: '0' }}>
+          <ParallaxLayer
+            sticky={{ start: 0, end: paragraphs.length }}>
+            <ChapterTitle title={title} />
+            <Button onClick={() => setCurrentTheme(currentTheme.name === 'Green' ? theme.blue : theme.green)}>{currentTheme.name}</Button>
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 0, end: 0.5 }} style={{ backgroundImage: `linear-gradient(${currentTheme.primary}, #e2e0df)`, zIndex: -2, backgroundSize: '100%' }} />
+          {paragraphs.map((paragraph) => (
+            <ParallaxLayer
+              key={paragraph.id}
+              offset={parseInt(paragraph.id) * 0.5}
+              speed={Math.random()}
+            >
+              <ChapterParagraph paragraph={paragraph.content} />
+            </ParallaxLayer>
+          ))}
+          <ParallaxLayer
+            sticky={{ start: 2, end: paragraphs.length / 2 + 1 }}
+            style={{
+              backgroundImage: `url(${backgroundImg})`,
+              backgroundSize: 'center',
+              zIndex: -1
+            }} />
+        </Parallax>
       </div>
     </ThemeProvider>
   );
